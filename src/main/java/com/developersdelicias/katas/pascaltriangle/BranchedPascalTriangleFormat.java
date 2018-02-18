01f1b984e6a102d013b48e69ca5aaefa1bce2401
Benjamin Cisneros <cisnerosbarraza@gmail.com>
2018-02-18 15:21:10 -0700

    Format algorithm

--- src/main/java/com/developersdelicias/katas/pascaltriangle/BranchedPascalTriangleFormat.java
+++ src/main/java/com/developersdelicias/katas/pascaltriangle/BranchedPascalTriangleFormat.java
@@ -7,6 +7,7 @@ class BranchedPascalTriangleFormat implements PascalTriangleFormat {
 	private static final String NEW_LINE = "\n";
 	private static final String BRANCH_CONNECTOR = "/ \\";
 	private static final RepeatedString BLANK = new RepeatedString(" ");
+	private static final RepeatedString BRANCH_CONNECTOR_WITH_BLANK = new RepeatedString(BRANCH_CONNECTOR + BLANK.times(1));
 	private static final String EMPTY_STRING = "";
 
 	@Override
@@ -45,11 +46,11 @@ class BranchedPascalTriangleFormat implements PascalTriangleFormat {
 			Iterator<PascalTriangleNode> level3NodeIterator = level3.iterator();
 			Iterator<PascalTriangleNode> level4NodeIterator = level4.iterator();
 			return BLANK.times(6) + level1NodeIterator.next().value() + NEW_LINE +
-					BLANK.times(5) + BRANCH_CONNECTOR + NEW_LINE +
+					BLANK.times(5) + BRANCH_CONNECTOR_WITH_BLANK.times(0) + BRANCH_CONNECTOR + NEW_LINE +
 					BLANK.times(4) + level2NodeIterator.next().value() + BLANK.times(3) + level2NodeIterator.next().value() + NEW_LINE +
-					BLANK.times(3) + BRANCH_CONNECTOR + BLANK.times(1) + BRANCH_CONNECTOR + NEW_LINE +
+					BLANK.times(3) + BRANCH_CONNECTOR_WITH_BLANK.times(1) + BRANCH_CONNECTOR + NEW_LINE +
 					BLANK.times(2) + level3NodeIterator.next().value() + BLANK.times(3) + level3NodeIterator.next().value() + BLANK.times(3) + level3NodeIterator.next().value() + NEW_LINE +
-					BLANK.times(1) + BRANCH_CONNECTOR + BLANK.times(1) + BRANCH_CONNECTOR + BLANK.times(1) + BRANCH_CONNECTOR + NEW_LINE +
+					BLANK.times(1) + BRANCH_CONNECTOR_WITH_BLANK.times(2) + BRANCH_CONNECTOR + NEW_LINE +
 					BLANK.times(0) + level4NodeIterator.next().value() + BLANK.times(3) + level4NodeIterator.next().value() + BLANK.times(3) + level4NodeIterator.next().value() + BLANK.times(3) + level4NodeIterator.next().value();
 		}
 
