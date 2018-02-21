a7439e814be963a0f14383cee06d14811f4f88e1
Edwin Garcia <ed@live.no>
2018-02-21 08:05:12 -0700

    constants added
    constants added

--- src/main/java/com/developersdelicias/App.java
+++ src/main/java/com/developersdelicias/App.java
@@ -1,22 +1,24 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package pascal;
+package com.developersdelicias;
 
 import java.util.Scanner;
 
 /**
+ * Hello world!
  *
- * @author Edwin Garcia
  */
-public class Pascal {
+public class App {
 
-    /**
-     * @param args the command line arguments
-     */
-   public static Boolean bigger = false;
+    public static final int LOWER_LIMIT=3; 
+    public static final int UPPER_LIMIT=14; 
+    public static final int LOWER_LIMIT_FORMAT=3;
+    public static final int UPPER_LIMIT_FORMAT=12;
+    public static final int BIGGER_LIMIT_LINES=-2;
+    public static final int NORMAL_LIMIT_LINES=-1;
+    public static final int PADDING_LEFT_LAST_NUMBER=4;
+    public static final int BIGGER_PADDING_LEFT_LAST_NUMBER=6;
+    public final static String BIGGER_NODE="/   \\ ";
+    public final static String NODE="/ \\ ";
+    public static Boolean bigger = false;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -27,27 +29,26 @@ public class Pascal {
         int[][] matriz = new int[lines][];
         matriz[0] = new int[]{1};
         /*format stuff*/
-        if (lines > 3 && lines < 14) {
-            lines = lines + lines - 3;
-        } else if(lines>13){
-            lines = lines + lines + 12;
+        if (lines > LOWER_LIMIT && lines < UPPER_LIMIT) {
+            lines = lines + lines - LOWER_LIMIT;
+        } else if(lines>UPPER_LIMIT){
+            lines = lines + lines + UPPER_LIMIT_FORMAT;
             bigger = true;
         }
         System.out.println(paddingBeLike("1", ++lines, false));
         for (int k = 1; k < matriz.length; k++) {
             matriz[k] = new int[k + 1];
             matriz[k][0] = matriz[k][k] = 1;
-            lines = bigger?lines - 2:lines-1;
+            lines = bigger?lines + BIGGER_LIMIT_LINES:lines+NORMAL_LIMIT_LINES;
             System.out.println(paddingBeLike(node(k), lines, false));
             System.out.print(paddingBeLike(("1"), --lines, false));
             for (int i = 1; i < k; i++) {
                 matriz[k][i] = matriz[k - 1][i - 1] + matriz[k - 1][i];
-                System.out.print(padLeft(matriz[k][i] + "", bigger?6:4));
+                System.out.print(padLeft(matriz[k][i] + "", bigger?BIGGER_PADDING_LEFT_LAST_NUMBER:PADDING_LEFT_LAST_NUMBER));
             }
-            System.out.println(padLeft("1",bigger?6:4));
+            System.out.println(padLeft("1",bigger?BIGGER_PADDING_LEFT_LAST_NUMBER:PADDING_LEFT_LAST_NUMBER));
         }
     }
-
     public static String padRight(String s, int n) {
         return String.format("%1$-" + n + "s", s);
     }
@@ -56,10 +57,10 @@ public class Pascal {
         return String.format("%1$" + n + "s", s);
     }
 
-    public static String node(int ramas) {
+    public static String node(int branches) {
         String slashes = "";
-        for (int i = 0; i < ramas; i++) {
-            slashes+=bigger?"/   \\ ":"/ \\ ";
+        for (int i = 0; i < branches; i++) {
+            slashes+=bigger?BIGGER_NODE:NODE;
         }
         return slashes;
     }
@@ -72,4 +73,3 @@ public class Pascal {
         return right ? (txt + space) : (space + txt);
     }        
 }
-
